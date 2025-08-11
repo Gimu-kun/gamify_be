332cef870d81f0d7283a073cf8fca1a7c6d7cfb0
Gimu-kun <hieunghiemle@gmail.com>
2025-08-11 12:09:49 +0700

    feature/create firebase storage

--- src/main/java/com/example/gamify_be/Config/FirebaseConfig.java
+++ src/main/java/com/example/gamify_be/Config/FirebaseConfig.java
@@ -1,4 +1,30 @@
 package com.example.gamify_be.Config;
 
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+
+import javax.annotation.PostConstruct;
+import java.io.IOException;
+import java.io.InputStream;
+
+@Configuration
 public class FirebaseConfig {
+    @PostConstruct
+    public void init() throws IOException {
+        InputStream serviceAccount =
+                new ClassPathResource("firebase/serviceAccountKey.json").getInputStream();
+
+        FirebaseOptions options = FirebaseOptions.builder()
+                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                .setStorageBucket("wander-stay.appspot.com")
+                .build();
+
+        if (FirebaseApp.getApps().isEmpty()) {
+            FirebaseApp.initializeApp(options);
+        }
+    }
 }
+
